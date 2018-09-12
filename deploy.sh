42d4264260c9720ca74fd9f74061eaba98fd2ec0
岩松 竜也 <tatsuya.iwamatsu@bizreach.co.jp>
2018-09-12 17:37:25 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && chmod +x skaffold
-./skaffold run
+/home/tatsuya.iwamatsu/skaffold run
